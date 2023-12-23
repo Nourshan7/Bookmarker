4bac686742bc76f74654da0adba424ab84b44bec
NOURSHAN <151036272+Nourshan7@users.noreply.github.com>
2023-12-23 22:39:24 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+var siteName = document.getElementById('siteName');
+var siteUrl = document.getElementById('siteUrl');
+var submitBtn = document.getElementById('submitBtn');
+var tbody = document.getElementById('tableContent');
+var arr;
+
+
+if (localStorage.bookmarks != null) {
+
+    arr = JSON.parse(localStorage.bookmarks)
+} else {
+    arr = [];
+}
+
+function create() {
+
+    var bookmarkContent = {
+        siteName: siteName.value,
+        siteUrl: siteUrl.value,
+    }
+    arr.push(bookmarkContent);
+    localStorage.setItem('bookmarks', JSON.stringify(arr))
+
+    show();
+    clearInput();
+}
+
+
+function show() {
+    var tbodyContent = ''
+    for (var i = 0; i < arr.length; i++) {
+        tbodyContent += `  
+      <tr>
+        <td>${i + 1}</td>
+
+        <td>${arr[i].siteName}</td>
+        <td><a target="_blank" class=" btn bg-success" href="https://www.${arr[i].siteUrl}"><i class="bi bi-eye"></i> Visit</a>
+        </td>
+        
+
+
+        <td><button onclick="deleteData(${i})" id="delete" class="btn bg-danger"><i class="bi bi-trash3"></i> Delete</button></td>
+
+      </tr>`;
+    }
+
+    tbody.innerHTML = tbodyContent;
+};
+show();
+
+
+function deleteData(i) {
+    arr.splice(i, 1);
+    show();
+}
+
+
+function visitt(i) {
+    console.log(i);
+
+}
+
+
+function clearInput() {
+    siteName.value = "";
+    siteUrl.value = "";
+}
